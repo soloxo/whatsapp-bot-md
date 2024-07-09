74e99eafa41016a0d670640a5e2492c99a31a44b
soloxo <117898872+soloxo@users.noreply.github.com>
2024-07-09 21:16:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const { stopInstance } = require('./lib/pm2')
 const start = async () => {
   logger.info(`levanter ${VERSION}`)
   try {
-    await DATABASE.authenticate({ retry: { max: 3 } })
-  } catch (error) {
-    console.error('Unable to connect to the database:', error.message, process.env.DATABASE_URL)
-    return stopInstance()
-  }
-  try {
     logger.info('Database syncing...')
     await DATABASE.sync()
     const bot = new Client()
